8e128a3feaa95009b273a424ae3ba39c9d25da0c
Enterprise Solutions <telerik.enterprise.solutions@gmail.com>
2015-05-17 16:59:53 +0000

    Format code.

--- Estimote Demo/scripts/app.js
+++ Estimote Demo/scripts/app.js
@@ -5,26 +5,26 @@
     // store a reference to the application object that will be created
     // later on so that we can use it if need be
     var app;
-    
+
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      navigator.splashscreen.hide();
+    document.addEventListener('deviceready', function () {
 
-      app = new kendo.mobile.Application(document.body, {
-        
-        // you can change the default transition (slide, zoom or fade)
-        transition: 'slide',
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        skin: 'flat',
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        navigator.splashscreen.hide();
 
-        // the application needs to know which view to load first
-        initial: 'views/beacons.html'
-      });
+        app = new kendo.mobile.Application(document.body, {
+
+            // you can change the default transition (slide, zoom or fade)
+            transition: 'slide',
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'flat',
+
+            // the application needs to know which view to load first
+            initial: 'views/beacons.html'
+        });
 
     }, false);
 
--- Estimote Demo/scripts/beacons.js
+++ Estimote Demo/scripts/beacons.js
@@ -2,29 +2,26 @@
 function onBeaconsReceived(result) {
     if (result.beacons && result.beacons.length > 0) {
         var msg = "<b>I found " + result.beacons.length + " beacons! Find them and scan the ISBN codes of the books to which they are attached to learn more about them.</b><br/>";
-        for (var i=0; i<result.beacons.length; i++) {
+        for (var i = 0; i < result.beacons.length; i++) {
             var beacon = result.beacons[i];
-            if(beacon.distance > 0){
+            if (beacon.distance > 0) {
                 msg += "<br/>";
-            
+
                 if (beacon.color !== undefined) {
                     msg += "There is a <b>" + beacon.color + "</b> beacon ";
                 }
-                
-            msg += "within " + beacon.distance + " meters of this location.<br/>";
-            }
-            else{
+
+                msg += "within " + beacon.distance + " meters of this location.<br/>";
+            } else {
                 msg += "...but it's too far to find. Try retracing your steps to find it.<br/>";
             }
-           
+
         }
-        
-    }
-    
-    else {
+
+    } else {
         var msg = "I haven't found a beacon just yet. Let's keep looking!"
     }
-    
+
     document.getElementById('beaconlog').innerHTML = msg;
 }
 
@@ -48,30 +45,30 @@ document.addEventListener('beaconsReceived', onBeaconsReceived, false);
                 window.estimote.stopRanging();
             }
         },
-               
-        scan: function () {
-            
-            cordova.plugins.barcodeScanner.scan(
-                
-            function (result) {
-                setTimeout(function() { 
-                    var url = 'http://www.lookupbyisbn.com/Search/Book/' + result.text + '/1';                
-                    var bookinfo = "<a href='#' onclick=window.open('" + url + "','_blank')>Learn more about this book</a>";
-                    document.getElementById('bookinfo').innerHTML = bookinfo;                    
-                }, 0);
-                
-                
-            },
-        
-            function (error) {
-                alert("Scanning failed: " + error);
-            }
-          )
 
-            
+        scan: function () {
+
+            cordova.plugins.barcodeScanner.scan(
+
+                function (result) {
+                    setTimeout(function () {
+                        var url = 'http://www.lookupbyisbn.com/Search/Book/' + result.text + '/1';
+                        var bookinfo = "<a href='#' onclick=window.open('" + url + "','_blank')>Learn more about this book</a>";
+                        document.getElementById('bookinfo').innerHTML = bookinfo;
+                    }, 0);
+
+
+                },
+
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+            )
+
+
         },
 
-        checkSimulator: function() {
+        checkSimulator: function () {
             if (window.navigator.simulator === true) {
                 alert('This plugin is not available in the simulator.');
                 return true;
@@ -82,7 +79,7 @@ document.addEventListener('beaconsReceived', onBeaconsReceived, false);
                 return false;
             }
         }
-        
+
     });
 
     app.beaconService = {
